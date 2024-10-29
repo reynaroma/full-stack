5d1dd2bbcfeb04cab87d8963e43a2298e207abbb
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-29 17:28:26 -0600

    Install the shadcn button and use it in the main page

--- notion-clone-youtube/app/page.tsx
+++ notion-clone-youtube/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="">Let&lsquo;s build an API productivity APP</main>
+    <main className="">
+      <h1>Let&lsquo;s build an API productivity APP</h1>
+      <Button>Click Me</Button>
+    </main>
   );
 }
