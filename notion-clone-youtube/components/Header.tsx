1b3bc2da10e7d96231852047616b643bc245139f
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-29 19:08:13 -0600

    Add a style on the header

--- notion-clone-youtube/components/Header.tsx
+++ notion-clone-youtube/components/Header.tsx
@@ -6,9 +6,9 @@ function Header() {
   const { user } = useUser();
 
   return (
-    <div>
+    <div className="flex items-center justify-between p-5">
       {user &&
-        <h1>{user?.firstName}
+        <h1 className="text-2xl">{user?.firstName}
           {`'s`} Space
         </h1>}
       {/* Breadcrumbs */}
