71fd6fc5044da769ccaed5b7074d24ea4ed99b58
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-29 20:03:44 -0600

    Add style on the sheet to make the sheet feature relevant

--- notion-clone-youtube/components/Sidebar.tsx
+++ notion-clone-youtube/components/Sidebar.tsx
@@ -13,25 +13,37 @@ import NewDocumentButton from "./NewDocumentButton";
 import { MenuIcon } from "lucide-react";
 
 function Sidebar() {
+
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+      {/* Documents */}
+      {/* List */}
+
+      {/* Shared with me */}
+      {/* List */}
+    </>
+  );
+
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <Sheet>
-        <SheetTrigger>
-          <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
-        </SheetTrigger>
-        <SheetContent side={"left"}>
-          <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
-            <div>
-              {/* options */}
-            </div>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>
+                {menuOptions}
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
       <div className="hidden md:inline">
-
-
-        <NewDocumentButton />
+        {menuOptions}
       </div>
     </div>
   )
