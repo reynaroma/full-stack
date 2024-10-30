154d814d37d8bd55953f4ae50e0282995b182a5d
Reyna May Roma <roma.reynamay@gmail.com>
2024-10-29 19:40:40 -0600

    Install the drawer for the sidebar from shadcn

--- notion-clone-youtube/components/Sidebar.tsx
+++ notion-clone-youtube/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import NewDocumentButton from "./NewDocumentButton"
 
 function Sidebar() {
   return (
-    <div className="p-2">
+    <div className="p-2 md:p-5 bg-gray-200 relative">
       <NewDocumentButton />
     </div>
   )
