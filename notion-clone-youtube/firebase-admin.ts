63735ba5791bd5eb458a5812a02fc23035349c59
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-04 14:34:28 -0600

    Initialize firebase app

--- notion-clone-youtube/firebase-admin.ts
+++ notion-clone-youtube/firebase-admin.ts
@@ -7,3 +7,15 @@ import {
 } from "firebase-admin/app";
 
 import { getFirestore } from "firebase-admin/firestore";
+
+const serviceKey = require("@/service_key.json");
+
+let app: App;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(serviceKey),
+  });
+} else {
+  app = getApp();
+}
