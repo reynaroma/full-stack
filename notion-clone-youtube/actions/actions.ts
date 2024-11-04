bfe01211f3522043ceee75de45c7a89c7be18a73
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-04 15:35:39 -0600

    Create a server action  to createNewDocument and file for types and global inside tye types folder

--- notion-clone-youtube/actions/actions.ts
+++ notion-clone-youtube/actions/actions.ts
@@ -1,1 +1,18 @@
 "use server";
+
+import { adminDb } from "@/firebase-admin";
+import { auth } from "@clerk/nextjs/server";
+import { title } from "process";
+
+export async function createNewDocument() {
+  auth().protect();
+
+  const { sessionClaims } = await auth();
+
+  const docCollectionRef = adminDb.collection("documents");
+  const docRef = await docCollectionRef.add({
+    title: "New Doc",
+  });
+
+  await adminDb.collection("users").doc(sessionClaims?.email!)
+};
