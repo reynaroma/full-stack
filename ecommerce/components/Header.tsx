1a1e4b50a549af81057c9389c24d238c073c8e9d
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-19 19:24:21 -0600

    Create basket link component and style it

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -3,6 +3,7 @@
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import Form from 'next/form';
+import { TrolleyIcon } from "@sanity/icons";
 function Header() {
 
   const { user } = useUser(); // Get the user object from the hook if the user is logged in or null if not
@@ -25,6 +26,16 @@ function Header() {
             type="text" name="query" placeholder="Search for products"
             className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl" />
         </Form>
+        <div>
+          <Link href="/basket"
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            <TrolleyIcon className="h-6 w-6" />
+            {/* Span item count once global staste is implemented */}
+            <span>My Basket</span>
+          </Link>
+          {/* User icon */}
+        </div>
       </div>
     </header>
   )
