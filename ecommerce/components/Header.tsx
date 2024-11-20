01da46b332475c590fb7f04fdeccaf84ca0b9944
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-19 21:31:47 -0600

    Add style on the header elements

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
       {/* top row */}
-      <div>
+      <div className="flex w-full flex-wrap justify-between items-center">
         <Link
           href="/"
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
@@ -30,7 +30,7 @@ function Header() {
             type="text" name="query" placeholder="Search for products"
             className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl" />
         </Form>
-        <div>
+        <div className="flex items-center space-x-4 mt4 sm:mt-0 flex-1 lg:flex-none">
           <Link href="/basket"
             className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
