fd2f4841243b0c9108505351043fb6387e0bd306
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-19 18:54:14 -0600

    Pull the user object from the hook to identify if there's user logged in or not

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
 function Header() {
+
+  const { user } = useUser(); // Get the user object from the hook if the user is logged in or null if not
+
   return (
-    <div>
-      Header
-    </div>
+    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+  
+    </header>
   )
 }
 
