2de7de3b336fa997fbc5980f196a529fdb3ba50d
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-19 18:54:33 -0600

    Add a Link for the logo of the website

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -9,7 +9,15 @@ function Header() {
 
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
-  
+      {/* top row */}
+      <div>
+        <Link
+          href="/"
+          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+        >
+          Shopr
+        </Link>
+      </div>
     </header>
   )
 }
