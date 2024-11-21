d84188fd33ef42864afc767979eb72e59f6633be
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-20 20:53:53 -0600

    Inspect and modify the typo on the styling of the elements

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -24,13 +24,13 @@ function Header() {
         </Link>
         <Form
           action="/search"
-          className="wfull sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
         >
           <input
             type="text" name="query" placeholder="Search for products"
             className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl" />
         </Form>
-        <div className="flex items-center space-x-4 mt4 sm:mt-0 flex-1 lg:flex-none">
+        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 lg:flex-none">
           <Link href="/basket"
             className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
