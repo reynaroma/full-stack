0d10dff8e2659a5e9e909ece07d449d9d311bf28
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-27 15:27:58 -0600

    Add orderType document schema to Sanity with orderNumber,stripeSessionId and stripeCustomerId

---
+++ ecommerce/sanity/schemaTypes/orderType.ts
@@ -0,0 +1,28 @@
+import { BasketIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const orderType = defineType({
+  name: "order",
+  title: "Order",
+  type: "document",
+  icon: BasketIcon,
+  fields:[
+    defineField({
+      name: "orderNumber",
+      title: "Order Number",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "stripeCheckoutSessionId",
+      title: "Stripe Checkout Session ID",
+      type: "string",
+    }),
+    defineField({
+      name: "stripeCustomerId",
+      title: "Stripe Customer ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
