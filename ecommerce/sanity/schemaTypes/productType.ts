a37207bf9eee37bf41357f10c1804024ca3c0fcc
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-26 18:30:07 -0600

    Add procut type document schema to Sanity

---
+++ ecommerce/sanity/schemaTypes/productType.ts
@@ -0,0 +1,14 @@
+import { TrolleyIcon } from "@sanity/icons";
+import { defineField } from "sanity";
+
+export default {
+  name: 'productType',
+  title: 'Product type',
+  type: 'document',
+  icon: TrolleyIcon,
+  fields: [
+    defineField({
+      
+    })
+  ],
+}
