76d5490ebb97d08d5d0ec52ae343937b692a45b4
Reyna May Roma <roma.reynamay@gmail.com>
2024-11-26 18:47:12 -0600

    Define the fields in the productType schema

--- ecommerce/sanity/schemaTypes/productType.ts
+++ ecommerce/sanity/schemaTypes/productType.ts
@@ -1,4 +1,5 @@
 import { TrolleyIcon } from "@sanity/icons";
+import { Rule } from "postcss";
 import { defineField } from "sanity";
 
 export default {
@@ -8,7 +9,50 @@ export default {
   icon: TrolleyIcon,
   fields: [
     defineField({
-      
-    })
+      name: "name",
+      title: "Product Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "image",
+      title: "Product Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      }
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0),
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" }}],
+    }),
+    defineField({
+      name: "stock",
+      title: "Stock",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0),
+    }),
   ],
 }
