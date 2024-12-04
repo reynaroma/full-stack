1d661f7520d9a624e9cc3cc78f1e4dd0dda26994
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-04 16:27:13 -0600

    Create read token and add validation for SANITY_API_READ_TOKEN

--- ecommerce/sanity/lib/live.ts
+++ ecommerce/sanity/lib/live.ts
@@ -6,6 +6,11 @@ import "server-only";
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
+const token = process.env.SANITY_API_READ_TOKEN;
+if (!token) {
+  throw new Error("Missing SANITY_API_READ_TOKEN");
+};
+
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
     // Live content is currently only available on the experimental API
