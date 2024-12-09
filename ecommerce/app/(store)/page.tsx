883b5925355c80fa9e251ae421aa47eb7cdc0161
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-09 17:24:43 -0600

    Remove the unnecessary comment on the main page

--- ecommerce/app/(store)/page.tsx
+++ ecommerce/app/(store)/page.tsx
@@ -6,11 +6,6 @@ export default async function Home() {
   const products = await getAllProducts();
   const categories = await getAllCategories();
 
-  // console.log(
-  //   crypto.randomUUID().slice(0, 5) +
-  //   `>>> Rendered the home page cache with ${products.length} products and ${categories.length} categories`
-  // );
-
   return (
     <div className="">
       <h1>Hello World</h1>
