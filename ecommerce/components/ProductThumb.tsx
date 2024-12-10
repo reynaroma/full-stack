6c3c62e9168657a3a3afbd00b80bc9b55892b7af
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-10 17:05:52 -0600

    Add a link on the ProductThumb then style it

--- ecommerce/components/ProductThumb.tsx
+++ ecommerce/components/ProductThumb.tsx
@@ -1,10 +1,14 @@
 import { Product } from "@/sanity.types"
+import Link from "next/link";
 
 const ProductThumb = ({ product }: { product: Product }) => {
   const isOutOfStock = product.stock != null && product.stock <= 0;
 
   return (
-    <div>{product.name}</div>
+    <Link
+      href={`/product/${product.slug?.current}`}
+      className={`group flex flex-col bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden ${isOutOfStock ? 'opacity-50' : ''}`}
+    >Product</Link>
   )
 }
 export default ProductThumb;
