1c7daed32400fecc9e62aa402450eb4c7379087b
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-09 19:34:54 -0600

    Import AnimatePresence and motion from framer-motion then wrap the whole component to configure the animation

--- ecommerce/components/ProductGrid.tsx
+++ ecommerce/components/ProductGrid.tsx
@@ -1,10 +1,23 @@
-import { Product } from "@/sanity.types"
+import { Product } from "@/sanity.types";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ProductGrid = ({ products }: { products: Product[] }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
       {products?.map((product) => {
-        return <ProductThumb key={product._id} product={product} />;
+        return (
+          <AnimatePresence key={product._id}>
+            <motion.div
+              layout
+              initial={{ opacity: 0.2 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="flex justify-center"
+            >
+              {/* <ProductThumb key={product._id} product={product} /> */}
+            </motion.div>
+          </AnimatePresence>
+        )
       })}
     </div>
   )
