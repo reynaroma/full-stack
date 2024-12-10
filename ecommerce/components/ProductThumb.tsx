f8b1d16fbe5837a36e6c00bc45a09f4173648013
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-10 16:10:34 -0600

    Check if the productIsOutOfStock

--- ecommerce/components/ProductThumb.tsx
+++ ecommerce/components/ProductThumb.tsx
@@ -1,6 +1,8 @@
 import { Product } from "@/sanity.types"
 
 const ProductThumb = ({ product }: { product: Product }) => {
+  const isProductOutOfStock = product.stock != null && product.stock <= 0;
+
   return (
     <div>{product.name}</div>
   )
