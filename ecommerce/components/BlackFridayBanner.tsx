c953515e8378aa6eb6db3525e9861d81354f75d0
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-13 17:12:44 -0600

    Add styling inside the Black Friday Banner

--- ecommerce/components/BlackFridayBanner.tsx
+++ ecommerce/components/BlackFridayBanner.tsx
@@ -4,7 +4,7 @@ async function BlackFridayBanner() {
   const sale = await getActiveSaleByCouponCode('BFRIDAY');
 
   return (
-    <div>
+    <div className="bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mx-4 mt-2 rounded-lg shadow-lg">
       Black Friday Banner
     </div>
   )
