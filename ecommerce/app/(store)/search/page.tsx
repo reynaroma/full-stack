473f015c1fbb9af65fef9efa97cfcc87ed2f45d4
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-13 19:32:07 -0600

    Create the search page main component when result matches the query

--- ecommerce/app/(store)/search/page.tsx
+++ ecommerce/app/(store)/search/page.tsx
@@ -1,3 +1,4 @@
+import ProductGrid from "@/components/ProductGrid";
 import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 
 async function SearchPage({
@@ -27,8 +28,13 @@ async function SearchPage({
   }
 
   return (
-    <div>
-      SearchPage for {query}
+    <div className="flex flex-col items-center justify min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search results for {query}
+        </h1>
+        <ProductGrid products={products} />
+      </div>
     </div>
   )
 }
