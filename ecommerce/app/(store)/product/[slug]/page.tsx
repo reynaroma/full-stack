0f845ac3a9b5f525b82cf42d965927e7864e609d
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-16 16:39:10 -0600

    Use await to be access params and change the type into Promise type

--- ecommerce/app/(store)/product/[slug]/page.tsx
+++ ecommerce/app/(store)/product/[slug]/page.tsx
@@ -1,4 +1,6 @@
-async function ProductPage({ params }: { params: { slug: string } }) {
+async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
   return (
     <div>
       ProductPage
