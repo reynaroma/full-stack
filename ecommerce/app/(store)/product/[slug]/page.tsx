ad8151b8aedc224ddc9baebf891609a32431cc50
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-16 17:35:08 -0600

    Add a condition to check if the product is available or out of stock

--- ecommerce/app/(store)/product/[slug]/page.tsx
+++ ecommerce/app/(store)/product/[slug]/page.tsx
@@ -1,17 +1,20 @@
+import ProductGrid from "@/components/ProductGrid";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { notFound } from "next/navigation";
 
 async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
-  
+
   if (!product) {
     return notFound();
-  }
+  };
+
+  const isOutOfStock = product.stock != null && product.stock <= 0;
 
   return (
     <div>
-      ProductPage
+      
     </div>
   )
 }
