04817d9adcc58bba6f2a22f5a545650cb7ad3ae7
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-16 17:04:57 -0600

    Create a helper function to fetch a product by slug then import it inside the ProductPage component

--- ecommerce/app/(store)/product/[slug]/page.tsx
+++ ecommerce/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
+
 async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
+  const product = await getProductBySlug(slug);
 
   return (
     <div>
