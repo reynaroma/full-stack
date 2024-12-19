81eef01e617d67c64503649d5c7782f9b4896a93
Reyna May Roma <roma.reynamay@gmail.com>
2024-12-18 18:27:01 -0600

    Create the popover component

--- ecommerce/components/ui/category-selector.tsx
+++ ecommerce/components/ui/category-selector.tsx
@@ -34,4 +34,24 @@ export function CategorySelectorComponent({
   const [value, setValue] = useState<string>("");
   const router = useRouter();
 
+  <Popover open={open} onOpenChange={setOpen}>
+    <PopoverTrigger asChild>
+      <Button
+        variant="outline"
+        role="combobox"
+        aria-expanded={open}
+        className="w-full max-w-full relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded"
+      >
+        {value
+          ? categories.find((category) => category._id === value)?.title
+          : "Filter by Category"}
+        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0" />
+      </Button>
+    </PopoverTrigger>
+    
+    <PopoverContent className="w-full p-0">
+
+    </PopoverContent>
+  </Popover>
+
 }
